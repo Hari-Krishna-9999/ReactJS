6f34a984eb172ca6812031eb265c6fa5cf4f84f2
23PA1A4584 <harikrishnamanam123@gmail.com>
2026-08-28 23:57:57 +0530

    First class

---
+++ app.js
@@ -0,0 +1,28 @@
+/**
+ * 
+ * 
+ * <div>
+ *    <div> 
+ *      <h1> Hello World! </h1>
+ *   </div>
+ * </div>   
+ */
+
+const parent = React.createElement('div', {id : "parent"},
+    [
+    React.createElement('div', {id : "child"},
+        [
+            React.createElement('h1', {id : "heading"}, 'Hello World h1 tag'),
+            React.createElement('h2', {id : "subheading"}, 'Hello World h2 tag')
+        ]
+    ),
+    React.createElement('div', {id : "child2"},
+        [
+            React.createElement('h1', {id : "heading"}, 'Hello World h1 tag'),
+            React.createElement('h2', {id : "subheading"}, 'Hello World h2 tag')
+        ]
+    )]
+);
+const heading = React.createElement('h1', {id : "heading" , xyz : "abc"}, 'Hello World!');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
